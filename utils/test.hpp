bd92c0ebfcc54df217d19f33fd91dbb64715f8a6
Michael Clausen <michael.clausen@hevs.ch>
2018-02-21 07:30:39 +0100

    Test output is colorized.

--- utils/test.hpp
+++ utils/test.hpp
@@ -4,6 +4,6 @@
 
 inline void test(const char* const name, std::function<bool()> function) {
     bool testPassed = function();
-    std::cout << name << ":" << ( testPassed ? " PASSED" : " FAILED") << "\n";
+    std::cout << name << ":" << ( testPassed ? " \033[32mPASSED\033[0m" : " \033[31mFAILED\033[0m") << "\n";
     if (!testPassed) exit(1);
 }
